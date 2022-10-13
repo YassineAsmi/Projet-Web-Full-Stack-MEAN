2884f5d20562f8117864bf0fc15e56b51714cdbf
khemaisismaill@gmail.com <khemaisismaill@gmail.com>
2022-10-13 21:39:30 +0100

    +-changed port

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const cors = require("cors");
-const cookieSession = require("cookie-session");
 var mongoose = require("mongoose");
 const app = express();
 
@@ -9,9 +7,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://weisman:1234@cluster0.0czqaho.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("Connected to MongoDB");
-  client.close();
+    const collection = client.db("test").collection("devices");
+    console.log("Connected to MongoDB");
+    client.close();
 });
 
 
@@ -23,9 +21,13 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
